8aaa67807f044068cc7d859c8f312295f8879a7f
FedericoCattani <federico.cattani1@gmail.com>
2023-03-17 16:55:56 +0100

    add to-do list in comment in js file

--- js/script.js
+++ js/script.js
@@ -7,6 +7,17 @@
 
 3.Poi chiedi il suo colore preferito;
 
-4.Infine scrivi sulla pagina nomecognomecolorepreferito23 
+4.Infine scrivi sulla pagina nomecognomecolorepreferito23.
+
+
+a)Inserire nome dell'utente in una var;
+
+b)Inserire cognome dell'utente in una var;
+
+c)Inserire colore preferito dell'utente in una var;
+
+d)Stampa pagina con nome, cognome, colore preferito, eta(?)'
 
 */
+
+
